db525c10f54d9ea0197927f3944497fd96ad9992
Benedicte Raae <raae@raae.codes>
2022-05-03 13:28:53 +0200

    feat: scrape crowdcast

---
+++ gatsby-node.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+
+exports.sourceNodes = async (gatsbyUtils) => {
+  const { actions, createNodeId, createContentDigest, reporter } = gatsbyUtils;
+  const { createNode } = actions;
+
+  try {
+    reporter.info("SOURCE CROWDCAST");
+
+    const { data } = await axios.get("https://app.scrapingbee.com/api/v1", {
+      params: {
+        api_key: process.env.SCRAPING_BEE_API_KEY,
+        url: "https://www.crowdcast.io/raae",
+        wait_for: ".profile-name",
+        extract_rules: {
+          name: ".profile-name",
+          webinars: {
+            selector: ".event-tile",
+            type: "list",
+            output: {
+              title: ".event-tile__title",
+              path: {
+                selector: "a",
+                output: "@href",
+              },
+            },
+          },
+        },
+      },
+    });
+
+    for (webinar of data.webinars) {
+      reporter.info("Create CrowdcastWebinar for " + webinar.path);
+
+      createNode({
+        id: createNodeId(webinar.path),
+        title: webinar.title,
+        url: "https://www.crowdcast.io/" + webinar.path,
+        rawScrape: webinar,
+        internal: {
+          type: `CrowdcastWebinar`,
+          mediaType: `text/json`,
+          content: JSON.stringify(webinar),
+          contentDigest: createContentDigest(webinar),
+        },
+      });
+    }
+  } catch (error) {
+    reporter.panic("SOURCE CROWDCAST Failed" + error.message);
+  }
+};
